2448609e43332a9a7664b7dba12843f7be729d32
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-06-16 19:29:21 -0400

    just last question left

--- index.js
+++ index.js
@@ -121,9 +121,10 @@ console.log(getWinnersByYear(getWinners, getYears));
 
 /* Task 7: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals(/* code here */) {
-
-    /* code here */
+function getAverageGoals(data) {
+    const initialValue = 0;
+    
+    data.reduce()
 
 };
 
